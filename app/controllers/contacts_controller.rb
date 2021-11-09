09e382a6006f9bfa5dbd4fc4e4e5e55c1ab2daf7
Amir998 <amir1b.6@gmail.com>
2021-11-10 00:25:33 +0100

    Add create/delete functionalities and flash messages

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,25 +4,40 @@ class ContactsController < ApplicationController
     @contacts = Contact.all#orderati ih po abecednom redu
   end
 
+  def edit#uraditi pomoću turbo-a, kreirati edit template i po potrebi ga renderati
+    respond_to do |format|
+      if @contact.save
+        flash[:notice] = "Added a new contact!"
+        format.turbo_stream
+      else
+        flash[:error] = "Failed to add a new contact!"
+        format.turbo_stream { render turbo_stream: turbo_stream.append(:flash, partial: "layouts/flash") }
+        #format.html { render :index, error: "Failed to add a new contact!" }
+      end
+    end
+  end
+
   def create
     @contact = Contact.new(contact_params)
 
     respond_to do |format|
       if @contact.save
-        format.html { status: :ok, notice: "Successfully added a new contact!" }
-        redirect_to root_path
+        flash[:notice] = "Added a new contact!"
+        format.turbo_stream
       else
-        format.html { render :index, error: "Failed to add a new contact!" }
+        flash[:error] = "Failed to add a new contact!"
+        format.turbo_stream { render turbo_stream: turbo_stream.append(:flash, partial: "layouts/flash") }
+        #format.html { render :index, error: "Failed to add a new contact!" }
       end
     end
   end
 
   def update
-    @contact = Contact.find(1)
+    @contact = Contact.find(params[:id])
 
     respond_to do |format|
       if @contact.update(contact_params)
-        format.html { status: :ok, notice: "Successfully updated a contact!" }
+        format.html { status :ok, notice: "Successfully updated a contact!" }
         redirect_to root_path
       else
         format.html { render :index, error: "Failed to update a contact!" }
@@ -31,12 +46,12 @@ class ContactsController < ApplicationController
   end
 
   def destroy
-    @contact = Contact.find(1)
+    @contact = Contact.find(params[:id])
 
     respond_to do |format|
       if @contact.destroy
-        format.html { notice: "Successfully deleted a contact!" }
-        redirect_to root_path
+        flash[:notice] = "Successfully deleted a contact!"
+        format.turbo_stream
       else
         format.html { render :index, error: "Failed to delete a contact!" }
       end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :contacts, only: [:index, :create, :update, :destroy]
-  
+  resources :contacts, only: [:index, :edit, :create, :update, :destroy]
+
   root to: "contacts#index"
 end
